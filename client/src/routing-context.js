45f82bcc30e1df2cd5fc4cd04288b7a94386e4a9
valdimar94 <valdimarjonsson94@gmail.com>
2016-12-18 14:29:12 +0000

    Api test set up in jenkins and should run

--- client/src/routing-context.js
+++ client/src/routing-context.js
@@ -15,7 +15,7 @@ function routingContext(injected){
     const environment = injected('env');
     var socketURI;
     if(environment==='development' || environment==='test'){
-        socketURI='http://localhost:8080'
+        socketURI='http://54.171.205.207/'
     } else {
         socketURI='/'
     }
